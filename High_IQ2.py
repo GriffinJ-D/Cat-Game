e16e2d4ac691c684707856a3e8bcbd04d43474db
GriffinJ-D <65211446+GriffinJ-D@users.noreply.github.com>
2020-11-10 21:27:41 -0800

    Delete High_IQ2.py

--- High_IQ2.py
+++
@@ -1,8 +0,0 @@
-class Player:
-    def __init__(self, X, Y, speedx, speedy, jumps, alive):
-        self.X = X
-        self.Y = Y
-        self.speedx = speedx
-        self.speedy = speedy
-        self.jumps = jumps
-        self.alive = alive
